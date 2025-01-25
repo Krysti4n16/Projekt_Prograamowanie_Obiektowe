6388550454eed2a65585676880d8a0381605d76a
SzymixuX <145720054+SzymixuX@users.noreply.github.com>
2025-01-25 01:18:10 +0100

    Poprawilem, teraz jablka spawnuja sie co dwie sekundy, ale maks 5 na mapie.

--- GamePanel.java
+++ GamePanel.java
@@ -5,6 +5,8 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GamePanel extends JPanel implements ActionListener {
 
@@ -13,20 +15,29 @@ public class GamePanel extends JPanel implements ActionListener {
     static final int UNIT_SIZE=25;
     static final int GAME_UNITS= (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
     static final int DELAY =75;
+     static final int FOOD_SPAWN_DELAY = 2000;
     final int x[] = new int[GAME_UNITS];
     final int y[] = new int[GAME_UNITS];
     int bodyParts=6;
     int applesEaten=0;
+    /*
     int appleX;
-    int appleY;
+    int appleY; CHYBA DO WYRZUCENIA BO NIE UZYWAMY NIGDZIE!
+    */
     char direction = 'R';
     boolean running = false;
     Timer timer;
+    Timer spawnowanieJedzenia;
     Random random;
+    List<Food> foods;
     Food currentFood; // Polimorficzne pole dla jedzenia
+
+
+
 
     GamePanel(){
         random= new Random();
+        foods = new ArrayList<>();
         this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
         this.setBackground(Color.black);
         this.setFocusable(true);
@@ -39,6 +50,51 @@ public class GamePanel extends JPanel implements ActionListener {
         running=true;
         timer= new Timer(DELAY, this);
         timer.start();
+
+        spawnowanieJedzenia = new Timer(FOOD_SPAWN_DELAY, e -> spawnFoodIfNeeded());
+        spawnowanieJedzenia.start();
+    }
+
+    public void newFood()
+    {
+        addFood();
+    }
+
+    public void spawnFoodIfNeeded()
+    {
+        if(foods.size() < 5)
+        {
+            addFood();
+        }
+    }
+
+    public void addFood()
+    {
+        int x = random.nextInt((int) (SCREEN_WIDTH / UNIT_SIZE)) * UNIT_SIZE;
+        int y = random.nextInt((int) (SCREEN_WIDTH / UNIT_SIZE)) * UNIT_SIZE;
+
+        if (random.nextBoolean())
+        {
+            foods.add(new RedApple(x, y));
+        }
+        else
+        {
+            foods.add(new BlueApple(x, y));
+        }
+    }
+
+    public void checkApple()
+    {
+        for(int i =0; i< foods.size(); i++)
+        {
+            Food food = foods.get(i);
+            if((x[0] == food.x) && (y[0] == food.y))
+            {
+                food.applyEffect(this);
+                foods.remove(i);
+                break;
+            }
+        }
     }
 
     public void paintComponent(Graphics g){
@@ -48,7 +104,10 @@ public class GamePanel extends JPanel implements ActionListener {
 
     public void draw(Graphics g) {
         if (running) {
-            currentFood.draw(g); // Rysowanie jedzenia
+            for(Food food : foods)
+            {
+                food.draw(g);
+            }
 
             for (int i = 0; i < bodyParts; i++) {
                 if (i == 0) {
@@ -68,19 +127,23 @@ public class GamePanel extends JPanel implements ActionListener {
             gameOver(g);
         }
     }
+    /*
     //Food currentFood;
     public void newFood() {
-        int x = random.nextInt((int) (SCREEN_WIDTH / UNIT_SIZE)) * UNIT_SIZE;
-        int y = random.nextInt((int) (SCREEN_HEIGHT / UNIT_SIZE)) * UNIT_SIZE;
+        foods.clear(); // Usuwanie starego jedzenia
+        int numberOfApples = 3; // Liczba jedzenia do wygenerowania
+        for (int i = 0; i < numberOfApples; i++) {
+            int x = random.nextInt((int) (SCREEN_WIDTH / UNIT_SIZE)) * UNIT_SIZE;
+            int y = random.nextInt((int) (SCREEN_HEIGHT / UNIT_SIZE)) * UNIT_SIZE;
 
-        // Losuj typ jedzenia
-        if (random.nextBoolean()) {
-            currentFood = new RedApple(x, y);
-        } else {
-            currentFood = new BlueApple(x, y);
+            if (random.nextBoolean()) {
+                foods.add(new RedApple(x, y)); // Dodaj czerwone jabłko
+            } else {
+                foods.add(new BlueApple(x, y)); // Dodaj niebieskie jabłko
+            }
         }
     }
-
+*/
     public void move(){
         for(int i=bodyParts;i>0;i--){
             x[i]=x[i-1];
@@ -101,14 +164,21 @@ public class GamePanel extends JPanel implements ActionListener {
                 break;
         }
     }
-
-    public void checkApple(){
-        if((x[0]==currentFood.x) && (y[0]== currentFood.y)){
-            currentFood.applyEffect(this);
-            newFood();
+/*
+    public void checkApple() {
+        for (int i = 0; i < foods.size(); i++) {
+            Food food = foods.get(i);
+            if ((x[0] == food.x) && (y[0] == food.y)) {
+                food.applyEffect(this); // Zastosowanie efektu
+                foods.remove(i); // Usuń zjedzone jabłko
+                break;
+            }
+        }
+        if (foods.isEmpty()) {
+            newFood(); // Jeśli wszystkie jabłka zostały zjedzone, generuj nowe
         }
     }
-
+*/
     public void checkCollisions(){
         //collisions head with body
         for(int i=bodyParts;i>0;i--){
